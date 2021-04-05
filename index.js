50d4c8ce1d041c4b0ac405e6e2d7cbb303b0afc6
Yosef <shmuli5770@gmail.com>
2021-04-05 11:11:31 -0400

    commit tree 10496cbbd8e44d577caac74a7e42f801dd053988

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result;
+  if (distance <= 400) {
+    result = 'This one is on me!'
+  } else if (distance > 2000) {
+    result = `I will gladly take your thirty bucks.`
+  }
+  if (distance > 2500) {
+    result = `No can do.`
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result;
+  city === 'NYC' ? result = 'Ok, sounds good.' : result = 'No go.';
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result;
+  switch(tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.'
+  }
+  return result;
 }
